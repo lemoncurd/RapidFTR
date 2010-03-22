bbdafc6c3c5f339e8dd350099b67e00bb990f2e7
Jen Smith <admin@users-macbook-pro-65.local>
2010-03-22 08:29:10 +0000

    started playing round with the history page

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,5 +2,6 @@ class HistoriesController < ApplicationController
   
   def show
     @child = Child.get(params[:child_id])
+      @histories = @child["histories"]
   end
 end
--- spec/views/histories/show.html.erb_spec.rb
+++ spec/views/histories/show.html.erb_spec.rb
@@ -4,13 +4,22 @@ describe "histories/show.html.erb" do
 
   describe "child history" do
 
+=begin
     it "should render only the creation record when no histories yet" do
-      assigns[:child] = Child.create(:last_known_location => "Haiti", :photo => uploadable_photo)
+      assigns[:created] = {:created_at => "Ten O Clock", :created_by => "Barney"}
       render
       response.should have_tag("li", :count => 1)
-      response.should have_tag("li", :text => /Record created by/)
+      response.should have_tag("li", :text => /Record created at Ten O Clock by Barney/)
     end
-    
+=end
+   it "should render only the creation record when no histories yet" do
+     assigns[:child] = Child.create(:last_known_location => "Haiti", :photo => uploadable_photo, :created_by => "Barney")
+     render
+     response.should have_tag("li", :count => 1)
+     response.should have_tag("li", :text => /Record created by/)
+    end
+
+ 
     it "should render photo change record when updating a photo" do
       child = Child.create(:last_known_location => "Haiti", :photo => uploadable_photo)
 
@@ -23,6 +32,18 @@ describe "histories/show.html.erb" do
 
       response.should have_tag("li", :count => 2)
       response.should have_tag("li", :text => /Photo changed/)
+    end
+    
+    it "renders the history partial for the histories" do
+      histories = ['change1', 'change2']
+      assigns[:histories] = histories
+      assigns[:child] = Child.create(:last_known_location => "Haiti", :photo => uploadable_photo)
+      template.should_receive(:render).with(
+              :partial => "history_item",
+              :collection => histories
+      )
+      
+      render
     end
   end
 end
