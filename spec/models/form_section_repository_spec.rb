9a9c6db543e9c0c0bd723e46ad15a4c67a093573
Tom Elkin <telkin@thoughtworks.com>
2010-03-27 16:25:11 +0000

    Fixed repository test

--- spec/models/form_section_repository_spec.rb
+++ spec/models/form_section_repository_spec.rb
@@ -1,20 +1,17 @@
-require "spec"
+require "spec_helper"
 
 describe "form section repository" do
 
   it "should return a form section for each form definition" do
 
-    #To change this template use File | Settings | File Templates.
-
     sectiondef1= FormSectionDefinition.new
     sectiondef2 = FormSectionDefinition.new
 
-    sectiondefs = FormSectionDefinition.stub!(:all).and_return([sectiondef1,sectiondef2])
+    FormSectionDefinition.stub!(:all).and_return([sectiondef1,sectiondef2])
 
     sections = FormSectionRepository.all
 
     sections.length.should == 2
-    
 
   end
 end
