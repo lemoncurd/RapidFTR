4bd5b92ef44f52d8367083c03c494ee191a7eb86
jennifersmith <jennifer.e.m.smith@gmail.com>
2010-03-27 13:47:36 +0000

    actually added our stuff

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ ActionController::Routing::Routes.draw do |map|
   map.login 'login', :controller=>'sessions',:action =>'new'
   map.logout 'logout', :controller=>'sessions',:action =>'destroy'
 
+  map.resources 'form_sections', :controller=>'form_section'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
 
